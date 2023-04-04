2006ac56cddf7519f1f3f2e94172fc5efd6ffecc
TarasShyshko <tarr13579@gmail.com>
2023-04-04 15:22:34 +0300

    Зверстав слайдер для адаптиву, та нормальний вигляд сторінки

--- js/script.js
+++ js/script.js
@@ -2,4 +2,22 @@ $(document).ready(function (){
     $('.menu_icon').click(function (event) {
         $('.menu_icon,.menu_body,.header').toggleClass('_active');
     });
+    $('.slider_blocks').slick({
+        slidesToShow: 3,
+
+        responsive:[
+            {
+                breakpoint: 1210,
+                settings: {
+                    adaptiveHeight: true,
+                    slidesToShow: 1,
+                    centerMode:true,
+                    fade: true,
+                    autoplay: true,
+                    speed:4000,
+                    dots: true,
+                }
+            }
+        ]
+    });
 });
